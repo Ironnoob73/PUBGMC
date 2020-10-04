7b698493086e9b8571201ca78709549f2bd67cdf
Toma <42510241+Toma1O6@users.noreply.github.com>
2020-10-04 19:57:24 +0200

    LivingEntity 3rd person gun holding animation

--- src/main/java/dev/toma/pubgmc/PubgmcHooks.java
+++ src/main/java/dev/toma/pubgmc/PubgmcHooks.java
@@ -1,12 +1,16 @@
 package dev.toma.pubgmc;
 
 import dev.toma.pubgmc.capability.player.InventoryFactory;
+import dev.toma.pubgmc.capability.player.PlayerCapFactory;
 import dev.toma.pubgmc.client.render.layer.BackpackLayer;
 import dev.toma.pubgmc.client.render.layer.GhillieLayer;
+import dev.toma.pubgmc.common.item.gun.GunItem;
 import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 
 public class PubgmcHooks {
 
@@ -22,7 +26,25 @@ public class PubgmcHooks {
         }
     }
 
+    /**
+     * Called from {@link BipedModel#setRotationAngles(LivingEntity, float, float, float, float, float, float)} just before return statement
+     * @param model Model being rendered
+     * @param entity Entity instance
+     */
     public static void onSetupRotationAngles(BipedModel<?> model, LivingEntity entity) {
-
+        boolean holdingWeapon = entity.getItemStackFromSlot(EquipmentSlotType.MAINHAND).getItem() instanceof GunItem;
+        if(holdingWeapon) {
+            if(entity instanceof PlayerEntity) {
+                boolean aiming = PlayerCapFactory.get((PlayerEntity) entity).getAimInfo().isAiming();
+            } else {
+                float f0 = (float) Math.toRadians(-90.0F);
+                float f1 = (float) Math.toRadians(-30.0F);
+                float f2 = (float) Math.toRadians(45.0F);
+                model.bipedRightArm.rotateAngleX = f0;
+                model.bipedRightArm.rotateAngleY = f1;
+                model.bipedLeftArm.rotateAngleX = f0;
+                model.bipedLeftArm.rotateAngleY = f2;
+            }
+        }
     }
 }
